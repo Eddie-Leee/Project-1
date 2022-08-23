9fffab822e45b32d254a53c53a3939af53fa0379
Eddie Lee <king55442001@gmail.com>
2022-08-23 21:38:02 +0800

    Upload Website2

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -48,10 +48,6 @@ const HomeComponent = () => {
             </div>
           </div>
         </div>
-
-        <footer className="pt-3 mt-4 text-muted border-top">
-          &copy; 2021 Wilson Ren
-        </footer>
       </div>
     </main>
   );
